fd271f1f52f1d3315e19b72530745d78397cde41
Tran.The.Anh.Tuan <tuan.tta1991@gmail.com>
2018-10-05 18:59:13 +0700

    Finish user edit, update, index and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create! name: "Example User", email: "example@railstutorial.org",
+  password: "111111", password_confirmation: "111111", admin: true
+
+User.create! name: "Tran The Anh Tuan", email: "hihihohohehe91@gmail.com",
+  password: "111111", password_confirmation: "111111", admin: true
+
+User.create! name: "Duong Phuong Thao", email: "duongthao.1610@gmail.com",
+  password: "111111", password_confirmation: "111111", admin: true
+
+99.times do |n|
+  name = FFaker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "banhxelangtu"
+  User.create! name: name, email: email, password: password,
+    password_confirmation: password
+end
